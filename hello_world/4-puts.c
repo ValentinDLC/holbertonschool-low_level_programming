cebf698222987b4f1e8f3fedcfa96871622f6b15
root <valentin.dardenne9@gmail.com>
2025-06-16 11:36:21 +0000

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Prints a quote about programming followed by a new line
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
