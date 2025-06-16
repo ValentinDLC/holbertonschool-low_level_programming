d55bbd4b4696164bc5c3c700e694b5268c4498d3
root <valentin.dardenne9@gmail.com>
2025-06-16 11:55:58 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a grammatically correct statement about art
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(" with proper grammar, but the the outcome is a piece of art,\n");
+	return (0);
+}
