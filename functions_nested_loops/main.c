a8200cd12b2f27a7469e0cc5299d07634e8f883e
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-17 17:59:18 +0200

    islower

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,8 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	print_alphabet_x10();
+	char test1 = 'a';
+	char test2 = 'Z';
+
+	printf("%c is lowercase? %d\n", test1, _islower(test1));
+	printf("%c is lowercase? %d\n", test2, _islower(test2));
+
 	return (0);
 }
 
