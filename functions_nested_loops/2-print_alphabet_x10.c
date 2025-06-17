4848bba9144004ed876459b4b2c757d00a8c9872
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-17 17:41:24 +0200

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char line[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 0;
+		while (line[j])
+		{
+			_putchar(line[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,8 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-
-#endif
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
 
