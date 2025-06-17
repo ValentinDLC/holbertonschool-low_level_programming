c62d0470574451bd9371bd1a3c264497129a9b91
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-17 11:32:59 +0200

    When I was having that alphabet soup, I never thought that it would pay off

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,19 +2,20 @@
 
 /**
  * main - prints the alphabet in lowercase, except q and e
- * Return:  Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char v = 'a';
-	
-while (v <= 'z')
-{
-if (v != 'e' && v != 'q')
-putchar(v);
-v++;
+	char v = 'a';
+
+	while (v <= 'z')
+	{
+		if (v != 'e' && v != 'q')
+			putchar(v);
+		v++;
+	}
+
+	putchar('\n');
+	return (0);
 }
 
-putchar('\n');
-return (0);
-}
