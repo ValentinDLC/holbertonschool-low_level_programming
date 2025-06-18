9c1a01bb25d41f15046fd577c2b8f1d9f89cd065
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-18 13:12:14 +0200

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,26 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point to test print_last_digit
+ * main - check the code
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(98);
-	_putchar('0' + r);
-	_putchar('\n');
-
-	r = print_last_digit(0);
-	_putchar('0' + r);
-	_putchar('\n');
-
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-
+	jack_bauer();
 	return (0);
 }
 
