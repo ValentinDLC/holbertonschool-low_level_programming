2812c2a56b17f8736d9598624bd91de8b5e792c9
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-18 11:49:11 +0200

    isalpha

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -3,11 +3,15 @@
 
 int main(void)
 {
-	char test1 = 'a';
-	char test2 = 'Z';
+	char c1 = 'b';
+	char c2 = 'Z';
+	char c3 = '3';
+	char c4 = '?';
 
-	printf("%c is lowercase? %d\n", test1, _islower(test1));
-	printf("%c is lowercase? %d\n", test2, _islower(test2));
+	printf("%c is alphabetic? %d\n", c1, _isalpha(c1));
+	printf("%c is alphabetic? %d\n", c2, _isalpha(c2));
+	printf("%c is alphabetic? %d\n", c3, _isalpha(c3));
+	printf("%c is alphabetic? %d\n", c4, _isalpha(c4));
 
 	return (0);
 }
