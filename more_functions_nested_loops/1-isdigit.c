59346291f9f0a9ccbc8fb87d5b0e46ae4950340f
ValentinDLC <valentin.dardenne9@gmail.com>
2025-06-23 11:20:45 +0200

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - cheks for a digit 0 through 9
+ * @c: the character to check
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
