a3046d5252bc0bf411fc6382bd3830be5a0f9e82
ValentinDLC <valentin.dardenne9@gmail.com>
2025-07-29 14:26:05 +0200

    betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,6 +2,12 @@
 
 #include "lists.h"
 
+/**
+ * list_len - Calcule la longueur d'une liste chaînée
+ * @h: Pointeur vers le premier nœud de la liste
+ *
+ * Return: Le nombre de nœuds dans la liste
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
@@ -11,5 +17,6 @@ size_t list_len(const list_t *h)
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
