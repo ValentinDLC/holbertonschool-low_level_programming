b67f41f6402cfe33a6433193f18635fe55c2d3f4
ValentinDLC <valentin.dardenne9@gmail.com>
2025-07-29 14:24:13 +0200

    List length

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
 
 /**
@@ -18,5 +19,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
